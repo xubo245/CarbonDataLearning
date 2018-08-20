48f740540506c4639fb73b680291a1811ee65bc5
xubo245 <601450868@qq.com>
2018-08-20 18:22:49 +0800

    wiki ETL

--- src/test/scala/org/github/xubo245/carbonDataLearning/etl/JudgeFile.scala
+++ src/test/scala/org/github/xubo245/carbonDataLearning/etl/JudgeFile.scala
@@ -7,11 +7,20 @@ import scala.io.Source
   */
 object JudgeFile {
   def main(args: Array[String]): Unit = {
-    val fileName = "/root/xubo/data/pageviews-20150505"
+//    val fileName = "/root/xubo/data/pageviews-20150505"
+
+//    val fileName = "/root/xubo/data/pageviews-20150505-000000WithTime"
+    for (i <- 0 to 9) {
+      val path = s"/root/xubo/data/pageviews-20150505-0" + i + "0000WithTime"
+      println("read file " +i + ":" + path)
+      judge(path)
+    }
+  }
+  def judge(fileName:String): Unit ={
     val reader = Source.fromFile(fileName)
     var i=1;
     for (line <- reader.getLines()) {
-      val array = line.split("\\s+")
+      val array = line.split('\t')
       if (array.length != 8) {
         println(i + ":\t" + line)
         i = i + 1
@@ -25,7 +34,12 @@ object JudgeFile {
       }
       if(line.length>3200){
         println(line)
+        println(line.length)
       }
+      if(array(5).equalsIgnoreCase("\"")){
+        println(line)
+      }
+
     }
   }
 }
--- src/test/scala/org/github/xubo245/carbonDataLearning/etl/WikiETL.scala
+++ src/test/scala/org/github/xubo245/carbonDataLearning/etl/WikiETL.scala
@@ -15,25 +15,30 @@ object WikiETL {
     for (file <- files.listFiles().sorted.filter(_.getCanonicalFile.getName.contains("pageviews"))) {
       val filePath = file.getCanonicalPath
       println(filePath)
-      //      val out = new PrintWriter(filePath + "WithTime")
+//            val out = new PrintWriter(filePath + "WithTime")
       val reader = Source.fromFile(filePath)
       val fileName = file.getCanonicalFile.getName
+      val delimiter="\t"
       for (line <- reader.getLines()) {
         val stringBuffer = new StringBuffer()
         stringBuffer
-          .append(fileName.substring(10, 14)).append("\t")
-          .append(fileName.substring(14, 16)).append("\t")
-          .append(fileName.substring(16, 18)).append("\t")
-          .append(fileName.substring(19, 21)).append("\t")
+          .append(fileName.substring(10, 14)).append(delimiter)
+          .append(fileName.substring(14, 16)).append(delimiter)
+          .append(fileName.substring(16, 18)).append(delimiter)
+          .append(fileName.substring(19, 21)).append(delimiter)
         val array=line.mkString.split("\\s+")
-        array.foreach(stringBuffer.append(_).append("\t"))
-        val result = stringBuffer.toString.substring(0, stringBuffer.length() - 1)
+        for (i <- 0 until array.length-1){
+          stringBuffer.append(array(i)).append(delimiter)
+        }
+        stringBuffer.append(array(array.length-1))
+//        val result = stringBuffer.toString.substring(0, stringBuffer.length() - 1)
         //        println(result)
-        if (array.length == 4 && array(2).matches("[0-9]*")) {
-          out.println(result)
+        if (array.length == 4 && array(2).matches("[0-9]*") && !array(1).contains("\"")) {
+//        if (array.length == 4 && array(2).matches("[0-9]*")) {
+          out.println(stringBuffer.toString)
         }
       }
-      //      out.close()
+//            out.close()
     }
     out.close()
   }
